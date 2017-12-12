edad40f7b068ed529b02b0264ed60f42e9e98526
MAC <mac@MACdeMacBook-Air.local>
2017-12-12 15:11:56 +0800

    add checkout routing

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -28,7 +28,11 @@ class Admin::ProductsController < ApplicationController
       render :edit
     end
   end
-  
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to admin_products_path
+  end
     private
     def product_params
       params.require(:product).permit(:title, :description, :quantity, :price, :image)
--- app/models/product.rb
+++ app/models/product.rb
@@ -14,4 +14,9 @@
 
 class Product < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :quantity, presence: true
+  validates :price, presence:true
+  validates :image, presence: true
 end
